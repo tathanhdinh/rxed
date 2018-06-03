bbb64fd497b9ed8dc0b42dc8787b50b938d45849
TA Thanh Dinh <tathanhdinh@gmail.com>
2018-06-04 01:27:56 +0200

    decode instruction

--- src/lib.rs
+++ src/lib.rs
@@ -16,17 +16,28 @@ pub fn get_version() -> String {
     vers.to_str().unwrap().to_owned()
 }
 
-#[derive(Debug)]
+#[derive(Default, Debug)]
 pub struct Error {
     xed_error: gen::xed_error_enum_t,
 }
 
 impl std::fmt::Display for Error {
-    fn fmt(&self, f: &mut std::fmt::Formatter ) -> std::fmt::Result {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         let err_str = unsafe {
             std::ffi::CStr::from_ptr(gen::xed_error_enum_t2str(self.xed_error))
         };
         write!(f, "{}", err_str.to_str().unwrap())
+    }
+}
+
+
+pub struct DecodedInstruction {
+    inst: gen::xed_decoded_inst_t,
+}
+
+impl std::fmt::Display for DecodedInstruction {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        unimplemented!()
     }
 }
 
@@ -41,6 +52,23 @@ impl Decoder {
         }
         let state = gen::xed_state_s { mmode, stack_addr_width };
         Decoder { state }
+    }
+
+    pub fn decode(itext: &[u8]) -> Result<DecodedInstruction, Error> {
+        let mut inst: gen::xed_decoded_inst_t = unsafe { 
+            let mut inst: gen::xed_decoded_inst_t = std::mem::zeroed();
+            gen::xed_decoded_inst_zero(&mut inst);
+            inst
+        };
+        let xed_error = unsafe {
+            gen::xed_decode(&mut inst, itext.as_ptr(), itext.len() as u32)
+        };
+        if xed_error == gen::XED_ERROR_NONE {
+            Ok(DecodedInstruction { inst })
+        }
+        else {
+            Err(Error { xed_error })
+        }
     }
 }
 
@@ -63,7 +91,5 @@ mod tests {
     fn xed_min() {
         let mmode_64 = gen::XED_MACHINE_MODE_LONG_64;
         let stack_addr_width_64 = gen::XED_ADDRESS_WIDTH_64b;
-
-
     }
 }
